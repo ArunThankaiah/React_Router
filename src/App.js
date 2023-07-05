28885a114543f742cceabe73de3756c18596d7f7
ArunThankaiah <arunchandhu120@gmail.com>
2023-07-05 18:11:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+export default function App() {
+  return <BrowserRouter>
+  <div>
+    <header>
+    <h1>Sample Project</h1>
+    <span style={{flex: 1}}/>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="gallery">Gallery</Link>
+    </nav>
+    </header>
+
+    <Routes>
+    <Route index element={<HomePage/>}/>
+    <Route path="about"  element={<AboutPage/>}/>
+    <Route path="gallery" element={<GalleryPage/>}/>
+    <Route path="*" element={<ErrorPage/>}/>
+    
+    
+      
+    
+
+    </Routes>
+   
+    
+
+    
+    
+  </div>
+  </BrowserRouter>
+  
 }
 
-export default App;
+function HomePage(){
+  return <div className="contents">
+  <h2>Home Page</h2>
+  <p>Hello I'am Arun Thankaiah</p>
+</div>;
+}
+
+function AboutPage(){
+  return <div className="contents">
+    <h2>About Page</h2>
+    <p>I'am a Full Stack Web Devloper</p>
+  </div>
+}
+
+function GalleryPage(){
+  return <div className="contents">
+    <h2>Gallery Page</h2>
+    
+  </div>
+}
+
+function ErrorPage(){
+  return <div className="contents">
+    <h2>404</h2>
+    <p>Page not found</p>
+    
+  </div>
+}
+
+
+
